ec7f91d5f65dd5c2547ceff2580260551275cc93
esther.tsai <esthertsai1204@gmail.com>
2025-05-12 02:01:04 +0800

    move get driver into conftest

--- conftest.py
+++ conftest.py
@@ -1,11 +1,19 @@
 import pytest
-from drivers.chrome_driver import get_driver
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 class Device:
     iPhoneX = "iPhone X"
     GalaxyS5 = "Galaxy S5"
 
+def get_driver(device_name):
+    mobile_emulation = { "deviceName": device_name }
+    options = Options()
+    options.add_experimental_option("mobileEmulation", mobile_emulation)
+    driver = webdriver.Chrome(options=options)
+    return driver
 
 @pytest.fixture
 def driver():
